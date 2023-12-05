1735fc7292ffc2ce062d1487b42ac7e7bc4396d4
Mandar Vasant Joshi <119092622+mandarjosh@users.noreply.github.com>
2023-12-05 23:27:09 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,11 +7,11 @@ const settings = {
 
 //SEO Related settings
 const seo = {
-  title: "Ashutosh's Portfolio",
+  title: "Mandar's Portfolio",
   description:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   og: {
-    title: "Ashutosh Hathidara Portfolio",
+    title: "Mandar Joshi's Portfolio",
     type: "website",
     url: "http://ashutoshhathidara.com/",
   },
